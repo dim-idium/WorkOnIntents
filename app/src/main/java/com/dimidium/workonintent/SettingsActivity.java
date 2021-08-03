126f71253b4825a64fc9a20d04c4e27ce241228e
dimidium <dim.khusnutdin@gmail.com>
2021-08-03 20:36:06 +0500

    add lisiner on the second activity

--- app/src/main/java/com/dimidium/workonintent/SettingsActivity.java
+++ app/src/main/java/com/dimidium/workonintent/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.dimidium.workonintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button btnReturn = findViewById(R.id.butt_return);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
