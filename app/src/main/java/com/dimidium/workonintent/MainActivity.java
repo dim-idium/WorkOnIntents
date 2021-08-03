fcd2e12794f2823cbd6e7bc89762e2f5b97c396b
dimidium <dim.khusnutdin@gmail.com>
2021-08-03 23:18:15 +0500

    add intent fof browser

--- app/src/main/java/com/dimidium/workonintent/MainActivity.java
+++ app/src/main/java/com/dimidium/workonintent/MainActivity.java
@@ -3,6 +3,7 @@ package com.dimidium.workonintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -51,6 +52,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button btnLink = findViewById(R.id.btn_link);
+        btnLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = getString(R.string.link);
+                Uri uri = Uri.parse(url);
+                Intent browser = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(browser);
+
+            }
+        });
+
 
     }
 }
